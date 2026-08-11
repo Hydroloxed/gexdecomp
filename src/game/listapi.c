8c1d6ac227dc148b2799f256aca5c48df44c1ec4
Hydrolox <me@hydrolox.me>
2026-08-11 21:05:13 +0200

    [listapi] Initial scaffolding

---
+++ src/game/listapi.c
@@ -0,0 +1,25 @@
+#include "listapi.h"
+
+// 0042cbb0
+void LST_InsertBefore(struct NodeType* oldnode, struct NodeType* newnode)
+{}
+
+// 0042cbd0
+void LST_InsertAfter(struct NodeType* oldnode, struct NodeType* newnode)
+{}
+
+// 0042cbf0
+void LST_Remove(struct NodeType* node)
+{}
+
+// 0042cc00
+void LST_AddTail(struct ListType* eelist, struct NodeType* node)
+{}
+
+// 0042cc20
+void LST_RemTail(struct ListType* eelist)
+{}
+
+// 0042cc50
+void LST_Init(struct ListType* eelist)
+{}
---
+++ src/game/listapi.h
@@ -0,0 +1,26 @@
+#ifndef LISTAPI_H
+#define LISTAPI_H
+
+struct NodeType
+{
+    struct NodeType* nd_next; // 00
+    struct NodeType* nd_prev; // 04
+    // 08
+};
+
+struct ListType
+{
+    struct NodeType* lst_head; // 00
+    struct NodeType* lst_tail; // 04
+    struct NodeType* lst_tailpred; // 08
+    // 0C
+};
+
+void LST_InsertBefore(struct NodeType* oldnode, struct NodeType* newnode);
+void LST_InsertAfter(struct NodeType* oldnode, struct NodeType* newnode);
+void LST_Remove(struct NodeType* node);
+void LST_AddTail(struct ListType* eelist, struct NodeType* node);
+void LST_RemTail(struct ListType* eelist);
+void LST_Init(struct ListType* eelist);
+
+#endif
