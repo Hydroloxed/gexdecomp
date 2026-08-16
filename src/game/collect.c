b20620fb91bfdd6842907e9afd7c7f057da12526
Hydrolox <me@hydrolox.me>
2026-08-16 18:53:27 +0200

    [collect] Initial scaffolding

---
+++ src/game/collect.c
@@ -0,0 +1,118 @@
+#include "collect.h"
+#include "unimplemented.h"
+
+// 0041a590
+int32 COL_GetRemoteStatus(int32 level)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 0041a600
+void COL_ClearStartDoor()
+{
+    UNIMPLEMENTED;
+}
+
+// 0041a610
+void COL_ClearStartDoors()
+{
+    UNIMPLEMENTED;
+}
+
+// 0041a630
+void COL_CollectAnItem(int32 coltype)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041a660
+void COL_ResetCollectibles()
+{
+    UNIMPLEMENTED;
+}
+
+// 0041a680
+int32 COL_RemoveOldVcrs()
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 0041a6f0
+void CameraInit(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041a810
+static void CameraFlashDraw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041a920
+void CameraDoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041ab00
+void CameraDraw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041ab10
+void CameraClid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041abb0
+void RemoteInit(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041ac70
+void RemoteDoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041aea0
+void RemoteClid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041b010
+void RemoteDraw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041b040
+void TapeInit(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041b070
+void TapeDoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041b0a0
+void TapeClid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041b0d0
+void TapeDraw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
---
+++ src/game/collect.h
@@ -0,0 +1,30 @@
+#ifndef COLLECT_H
+#define COLLECT_H
+#include "platform.h"
+
+struct CollideInfo;
+struct GXObject;
+
+int32 COL_GetRemoteStatus(int32 level);
+void COL_ClearStartDoor();
+void COL_ClearStartDoors();
+void COL_CollectAnItem(int32 coltype);
+void COL_ResetCollectibles();
+int32 COL_RemoveOldVcrs();
+
+void CameraInit(struct GXObject* gob, BOOL removed);
+void CameraDoIt(struct GXObject* gob);
+void CameraDraw(struct GXObject* gob);
+void CameraClid(struct GXObject* gob, struct CollideInfo* ci);
+
+void RemoteInit(struct GXObject* gob, BOOL removed);
+void RemoteDoIt(struct GXObject* gob);
+void RemoteClid(struct GXObject* gob, struct CollideInfo* ci);
+void RemoteDraw(struct GXObject* gob);
+
+void TapeInit(struct GXObject* gob, BOOL removed);
+void TapeDoIt(struct GXObject* gob);
+void TapeClid(struct GXObject* gob, struct CollideInfo* ci);
+void TapeDraw(struct GXObject* gob);
+
+#endif
