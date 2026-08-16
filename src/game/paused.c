1b2484698665bf8516eb80ed10e75f2c15d2a949
Hydrolox <me@hydrolox.me>
2026-08-16 19:21:03 +0200

    [paused] Initial scaffolding

---
+++ src/game/paused.c
@@ -0,0 +1,14 @@
+#include "paused.h"
+#include "unimplemented.h"
+
+// 0041beb0
+static void FUN_0041beb0(void)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041bfc0
+void ProcessPaused(struct GXObject* playerob)
+{
+    UNIMPLEMENTED;
+}
---
+++ src/game/paused.h
@@ -0,0 +1,8 @@
+#ifndef PAUSED_H
+#define PAUSED_H
+
+struct GXObject;
+
+void ProcessPaused(struct GXObject* playerob);
+
+#endif
