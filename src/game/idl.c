cbbe3775212e3e735fa163504a92ae526472915d
Hydrolox <me@hydrolox.me>
2026-08-17 14:35:47 +0200

    [idl] Initial scaffolding

---
+++ src/game/idl.c
@@ -0,0 +1,27 @@
+#include "platform.h"
+#include "unimplemented.h"
+
+// 0041fcb0
+static void OpenIdle(int32 idlidx)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041fd00
+BOOL IDL_Load(void)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 0041fd70
+void IDL_Free(void)
+{
+    UNIMPLEMENTED;
+}
+
+// 0041fdb0
+void IDL_SetIdle(int32 idle)
+{
+    UNIMPLEMENTED;
+}
---
+++ src/game/idl.h
@@ -0,0 +1,9 @@
+#ifndef IDL_H
+#define IDL_H
+#include "platform.h"
+
+BOOL IDL_Load(void);
+void IDL_Free(void);
+void IDL_SetIdle(int32 idle);
+
+#endif
