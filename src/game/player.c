9aa4661783f9a7d0d2b6b513dbd19879e132ab43
Hydrolox <me@hydrolox.me>
2026-08-25 12:31:36 +0200

    [player] Initial scaffolding

---
+++ src/game/player.c
@@ -0,0 +1,765 @@
+#include "player.h"
+#include "platform.h"
+#include "unimplemented.h"
+
+// 00410d10
+void FUN_00410d10(uint32 param_1)
+{
+    UNIMPLEMENTED;
+}
+
+// 00410dc0
+void PlayerSetPropertiesForCameraScroll(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411160
+void InitPlayerSideCrawl(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411230
+void FUN_00411230(struct GXObject* gob, struct FrameInfo* param_2, int32* param_3, int32* param_4)
+{
+    UNIMPLEMENTED;
+}
+
+// 004112e0
+void FUN_004112e0(struct GXObject* gob, uint32 param_2)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411380
+void PlayerPlatSideCrawl(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411a40
+void InitPlayerPlatSideCrawl(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411a90
+void PlayerSideInside45Trans(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411b90
+void InitPlayerSideInside45Trans(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411c40
+void PlayerSideInside45TransBackward(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411c50
+void InitPlayerInside45TransBackward(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411c60
+void PlayerSideOutside45Trans(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411d70
+void InitPlayerSideOutside45Trans(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411e20
+void PlayerSideOutside45TransBackward(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411e30
+void InitPlayerSideOutside45TransBackward(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411e40
+void PlayerSideInside90Trans(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411fd0
+void InitPlayerSideInside90Trans(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00411ff0
+void FUN_00411ff0(struct GXObject* gob, int32 param_2, int32 param_3)
+{
+    UNIMPLEMENTED;
+}
+
+// 004120c0
+void FUN_004120c0(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004121b0
+void FUN_004121b0(struct GXObject* gob, BOOL param_2, BOOL param_3)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412270
+void PlayerSideInside90TransBackward(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412280
+void InitPlayerSideInside90TransBackward(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412290
+void PlayerSideOutside90Trans(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004123c0
+void InitPlayerSideOutside90Trans(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004124a0
+void PlayerSideOutside90TransBackward(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004124b0
+void InitPlayerSideOutside90TransBackward(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004124c0
+void PlayerSideGetup(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004125b0
+void InitPlayerSideGetup(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412630
+void PlayerSideUTurn(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412750
+void InitPlayerSideUTurn(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412800
+void PlayerPlatSideGetup(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412810
+void InitPlayerPlatSideGetup(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412830
+void PlayerPlatSideOutside90Trans(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412840
+void InitPlayerOutside90Trans(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412860
+void PlayerPlatSideOutside90TransBackward(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412870
+void InitPlayerPlatSideOutside90TransBackward(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412880
+void PlayerSideTongueLash(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412960
+void InitPlayerSideTongueLash(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412a00
+void FUN_00412a00(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412a50
+void PlayerFaceSpin(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412af0
+void InitPlayerFaceSpin(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412b30
+void PlayerFaceWiggle(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412b40
+void InitPlayerFaceWiggle(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412b50
+void PlayerFaceSpinAround(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412d00
+void InitPlayerFaceSpinAround(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412de0
+void PlayerFaceUnspin(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412ee0
+void InitPlayerFaceUnspin(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00412f40
+void PlayerSideTongueLash90(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413050
+void InitPlayerSideTongueLash90(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004130a0
+void PlayerSideSpin(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413170
+void InitPlayerSideSpin(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413230
+void PlayerSideSpinAround(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413350
+void InitPlayerSideSpinAround(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004133a0
+void PlayerSideUnspin(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413470
+void InitPlayerSideUnspin(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004134b0
+void PlayerSideSlap(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004135a0
+void InitPlayerSideSlap(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413600
+void PlayerSideTailWhap(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004136d0
+void InitPlayerTailWhap(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413730
+void PlayerSideJump(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004138b0
+void InitPlayerSideJump(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413ac0
+void PlayerFaceTongueLash(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413ba0
+void InitPlayerFaceTongueLash(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413be0
+void PlayerTailBounce(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413c80
+void InitPlayerTailBounce(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413d60
+void PlayerBounceFall(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413e60
+void InitPlayerBounceFall(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413ed0
+void PlayerBounceUnFall(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413ee0
+void InitPlayerBounceUnFall(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00413f10
+void PlayerBounceRise(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414030
+void InitPlayerBounceRise(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004140f0
+void PlayerAirToSideCrawl(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414130
+void InitPlayerAirToSideCrawl(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414240
+void PlayerPlatAirToSideCrawl(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414290
+void InitPlayerPlatAirToSideCrawl(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414330
+void PlayerAirToFaceCrawl(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414370
+void InitPlayerAirToFaceCrawl(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004143b0
+void PlayerFaceCrawlToAir(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004144e0
+void InitPlayerFaceCrawlToAir(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414600
+void PlayerLookup(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004146d0
+void InitPlayerLookup(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414710
+void PlayerTongueUp(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414890
+void InitPlayerTongueUp(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414900
+void PlayerDuckTongueLash(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414a30
+void InitPlayerDuckTongueLash(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414a80
+void PlayerDuckSwallow(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414b60
+void InitPlayerDuckSwallow(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414bb0
+void PlayerDuckSpin(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414c90
+void InitPlayerDuckSpin(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414ce0
+void PlayerDuckSpinAround(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414e20
+void InitPlayerDuckSpinAround(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414e80
+void PlayerDuckUnspin(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414f60
+void InitPlayerDuckUnspin(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00414fa0
+void PlayerFaceSwallow(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415040
+void InitPlayerFaceSwallow(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415080
+void PlayerSideSwallow(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415120
+void InitPlayerSideSwallow(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415170
+void FUN_00415170(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004152f0
+void FUN_004152f0(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004153e0
+void FUN_004153e0(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415480
+void FUN_00415480(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004155a0
+void PlayerStartFromCamera(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004155c0
+void InitPlayerStartFromCamera(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415600
+void PlayerLaunch(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415650
+void InitPlayerLaunch(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004156e0
+void PlayerLaunchTailBounce(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415720
+void InitPlayerLaunchTailBounce(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415790
+void PlayerShrinkToTube(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004157e0
+void InitPlayerShrinkToTube(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415820
+void PlayerGoThruTube(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415b20
+void InitPlayerGoThruTube(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415b80
+void PlayerComeOutTube(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415b90
+void InitPlayerComeOutTube(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415bb0
+static void GoToMap(void)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415c10
+void PlayerDoit2(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00415e80
+void PlayerDoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00416320
+void FUN_00416320(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00417500
+void PlayerDraw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00417780
+void PlayerInit(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00417920
+void ob302Clid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 00417960
+void ob303Clid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 00417970
+void PlayerClid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 00417b70
+void PlayerDamage(void)
+{
+    UNIMPLEMENTED;
+}
+
+// 00417ca0
+void PlayerKill(void)
+{
+    UNIMPLEMENTED;
+}
+
+// 00417d90
+void FUN_00417d90(void)
+{
+    UNIMPLEMENTED;
+}
+
+// 00417e70
+void ResetPlayerHP(void)
+{
+    UNIMPLEMENTED;
+}
+
+// 00417ee0
+void InitPlayer(void)
+{
+    UNIMPLEMENTED;
+}
