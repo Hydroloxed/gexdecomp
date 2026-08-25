aa0b16887c3c0a05ee07704504b2b34949408743
Hydrolox <me@hydrolox.me>
2026-08-25 18:53:36 +0200

    [map] Initial scaffolding

---
+++ src/game/map.c
@@ -0,0 +1,265 @@
+#include "map.h"
+#include "platform.h"
+#include "unimplemented.h"
+#include <stddef.h>
+
+// 00429190
+int32 FUN_00429190(int param_1, int param_2, int param_3, int param_4)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 00429200
+void PasswordGet(char* password)
+{
+    UNIMPLEMENTED;
+}
+
+// 00429390
+BOOL PasswordDecode(char* param_1)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 004295c0
+BOOL PasswordIsValid(char* param_1)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 00429690
+void PasswordGetChars(char* param_1)
+{
+    UNIMPLEMENTED;
+}
+
+// 004296d0
+void PasswordPutBits(char* param_1, uint32 value, uint32 first, uint32 num)
+{
+    UNIMPLEMENTED;
+}
+
+// 004297b0
+int PasswordDecodeGetBits(char* param_1, uint32 first, uint32 num)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 00429850
+void FUN_00429850(char* param_1, uint8* param_2, int param_3)
+{
+    UNIMPLEMENTED;
+}
+
+// 004298c0
+int32 FUN_004298c0(char* param_1, char* param_2)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 00429940
+void PasswordEnterLevel(void)
+{
+    UNIMPLEMENTED;
+}
+
+// 00429a00
+void ob220Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00429ac0
+void ob220DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00429b30
+void ob220Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00429b40
+struct GXObject* RemoteFindWithLevel(int32 level)
+{
+    UNIMPLEMENTED;
+    return NULL;
+}
+
+// 00429b80
+struct GXObject* FUN_00429b80(void)
+{
+    UNIMPLEMENTED;
+    return NULL;
+}
+
+// 00429bd0
+struct GXObject* FUN_00429bd0(void)
+{
+    UNIMPLEMENTED;
+    return NULL;
+}
+
+// 00429c10
+struct GXObject* FUN_00429c10(uint32 param_1)
+{
+    UNIMPLEMENTED;
+    return NULL;
+}
+
+// 00429c60
+struct GXObject* GOB_FindFirstWithType(uint32 gobtype)
+{
+    UNIMPLEMENTED;
+    return NULL;
+}
+
+// 00429c90
+int32 FUN_00429c90(void)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 00429cb0
+void FUN_00429cb0(int param_1, int param_2)
+{
+    UNIMPLEMENTED;
+}
+
+// 00429ce0
+void MapVcrDoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042a560
+int FUN_0042a560(struct GXObject* param_1, int param_2)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 0042a5d0
+int FUN_0042a5d0(struct GXObject* param_1, int param_2)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 0042a630
+BOOL FUN_0042a630(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 0042a690
+void MapVcrDraw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042ab90
+void ob262Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042aba0
+void ob262DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042acb0
+void ob262Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042ad00
+void ob270Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042ad40
+void ob270DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042b180
+void ob270Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042b530
+void MapPlayerInit(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042b760
+void FUN_0042b760(int32 x, int32 y)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042b7f0
+void MapPlayerDoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042bfe0
+int FUN_0042bfe0(struct GXObject* gob, int32 param_2)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 0042c0a0
+void FUN_0042c0a0(struct GXObject* param_1, struct GXObject* param_2, int param_3)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042c1a0
+void FUN_0042c1a0(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042c230
+void MapPlayerDraw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042c860
+void FUN_0042c860(struct GXObject* param_1, int param_2, int param_3)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042ca40
+void FUN_0042ca40(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042cba0
+void MapPlayerClid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
---
+++ src/game/map.h
@@ -0,0 +1,50 @@
+#ifndef MAP_H
+#define MAP_H
+#include "platform.h"
+
+struct CollideInfo;
+struct GXObject;
+
+int32 FUN_00429190(int param_1, int param_2, int param_3, int param_4);
+void PasswordGet(char* password);
+BOOL PasswordDecode(char* param_1);
+BOOL PasswordIsValid(char* param_1);
+void PasswordGetChars(char* param_1);
+void PasswordPutBits(char* param_1, uint32 value, uint32 first, uint32 num);
+int PasswordDecodeGetBits(char* param_1, uint32 first, uint32 num);
+void FUN_00429850(char* param_1, uint8* param_2, int param_3);
+int32 FUN_004298c0(char* param_1, char* param_2);
+void PasswordEnterLevel(void);
+void ob220Init(struct GXObject* gob, BOOL removed);
+void ob220DoIt(struct GXObject* gob);
+void ob220Draw(struct GXObject* gob);
+struct GXObject* RemoteFindWithLevel(int32 level);
+struct GXObject* FUN_00429b80(void);
+struct GXObject* FUN_00429bd0(void);
+struct GXObject* FUN_00429c10(uint32 param_1);
+struct GXObject* GOB_FindFirstWithType(uint32 gobtype);
+int32 FUN_00429c90(void);
+void FUN_00429cb0(int param_1, int param_2);
+void MapVcrDoIt(struct GXObject* gob);
+int FUN_0042a560(struct GXObject* param_1, int param_2);
+int FUN_0042a5d0(struct GXObject* param_1, int param_2);
+BOOL FUN_0042a630(struct GXObject* gob);
+void MapVcrDraw(struct GXObject* gob);
+void ob262Init(struct GXObject* gob, BOOL removed);
+void ob262DoIt(struct GXObject* gob);
+void ob262Draw(struct GXObject* gob);
+void ob270Init(struct GXObject* gob, BOOL removed);
+void ob270DoIt(struct GXObject* gob);
+void ob270Draw(struct GXObject* gob);
+void MapPlayerInit(struct GXObject* gob, BOOL removed);
+void FUN_0042b760(int32 x, int32 y);
+void MapPlayerDoIt(struct GXObject* gob);
+int FUN_0042bfe0(struct GXObject* gob, int32 param_2);
+void FUN_0042c0a0(struct GXObject* param_1, struct GXObject* param_2, int param_3);
+void FUN_0042c1a0(struct GXObject* gob);
+void MapPlayerDraw(struct GXObject* gob);
+void FUN_0042c860(struct GXObject* param_1, int param_2, int param_3);
+void FUN_0042ca40(struct GXObject* gob);
+void MapPlayerClid(struct GXObject* gob, struct CollideInfo* ci);
+
+#endif
