bb7ac0c137e19607d1e851690f9dc05cc5af7cac
Hydrolox <me@hydrolox.me>
2026-08-25 12:52:45 +0200

    [levselct] Initial scaffolding

---
+++ src/game/levselct.c
@@ -0,0 +1,7 @@
+#include "unimplemented.h"
+
+// 00420300
+void DoLevelSelectScreen(void)
+{
+    UNIMPLEMENTED;
+}
---
+++ src/game/levselct.h
@@ -0,0 +1,6 @@
+#ifndef LEVSELCT_H
+#define LEVSELCT_H
+
+void DoLevelSelectScreen(void);
+
+#endif
