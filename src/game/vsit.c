4d5600a3a6cfec1f0ea629b2be8de99892db91d2
Hydrolox <me@hydrolox.me>
2026-08-25 12:47:36 +0200

    [vsit] Improve formatting
    Should now be found by report.py

--- src/game/vsit.c
+++ src/game/vsit.c
@@ -1,16 +1,29 @@
 // This file is TODO
 // I want to find research these functions more before decomping them.
 
-// VFX_Reset	0041f840	undefined VFX_Reset(void)	28
-// VSIT_MarkForPlay	0041f860	undefined VSIT_MarkForPlay(undefined4 param_1, undefined4 param_2)	67
-// FUN_0041f8b0	0041f8b0	undefined FUN_0041f8b0(void)	1
-// VSIT_PlayVoiceSituation	0041f8c0	undefined VSIT_PlayVoiceSituation(VSITID vsit)	138
-// VFX_UpdatePlaying	0041f950	undefined VFX_UpdatePlaying(void)	187
-// FUN_0041fa10	0041fa10	undefined4 FUN_0041fa10(void)	104
-// VFX_Play	0041fa80	undefined4 VFX_Play(VSITID param_1)	107
-// VFX_Update	0041faf0	undefined VFX_Update(void)	92
-// VFX_VoiceFinished	0041fb50	int VFX_VoiceFinished(void)	34
-// VSIT_ForceVoiceSituation	0041fb80	undefined VSIT_ForceVoiceSituation(int param_1)	30
-// VSIT_ForcedVoiceSituationReady	0041fba0	int VSIT_ForcedVoiceSituationReady(void)	18
-// VSIT_UnforceVoiceSituation	0041fbc0	undefined VSIT_UnforceVoiceSituation(void)	13
-// ForceVSITInit	0041fbd0	undefined ForceVSITInit(GXObject * param_1)	75
+// 0041f840
+// undefined VFX_Reset(void)
+// 0041f860
+// undefined VSIT_MarkForPlay(undefined4 param_1, undefined4 param_2)
+// 0041f8b0
+// undefined FUN_0041f8b0(void)
+// 0041f8c0
+// undefined VSIT_PlayVoiceSituation(VSITID vsit)
+// 0041f950
+// undefined VFX_UpdatePlaying(void)
+// 0041fa10
+// undefined4 FUN_0041fa10(void)
+// 0041fa80
+// undefined4 VFX_Play(VSITID param_1)
+// 0041faf0
+// undefined VFX_Update(void)
+// 0041fb50
+// int VFX_VoiceFinished(void)
+// 0041fb80
+// undefined VSIT_ForceVoiceSituation(int param_1)
+// 0041fba0
+// int VSIT_ForcedVoiceSituationReady(void)
+// 0041fbc0
+// undefined VSIT_UnforceVoiceSituation(void)
+// 0041fbd0
+// undefined ForceVSITInit(GXObject * param_1)
