f499c03772b5d4169b7c08aaf4385836ca918586
Hydrolox <me@hydrolox.me>
2026-08-28 17:45:32 +0200

    [generic] Initial scaffolding

---
+++ src/game/generic.c
@@ -0,0 +1,767 @@
+#include "generic.h"
+#include "platform.h"
+#include "unimplemented.h"
+#include <stddef.h>
+
+// 0042e2b0
+void JumpFXDraw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e480
+void EFECT_AddPuff(int32 x, int32 y, uint32 flags, int32 angle, uint32 prio)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e550
+void ob288Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e560
+void ob288DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e570
+void ob288Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e580
+void ob289Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e590
+void ob289DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e5a0
+void ob289Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e5b0
+void ob290Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e5c0
+void ob290DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e5d0
+void ob290Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e5e0
+void FUN_0042e5e0(struct GXObject* param_1, int32 x, int32 y)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e660
+void FUN_0042e660(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// TODO: I think these are math/position helpers? Have to figure out what
+//       DAT_0045b104 and DAT_0045b108 do...
+
+// 0042e720
+int FUN_0042e720(int32 param_1, int32 param_2)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 0042e750
+int FUN_0042e750(int32 param_1, int32 param_2)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 0042e780
+void FUN_0042e780(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e850
+void FUN_0042e850(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042e8b0
+BOOL FUN_0042e8b0(struct GXObject* param_1, int param_2)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 0042e930
+void FUN_0042e930(int32 param_1, int32 param_2, int32 param_3, uint32 param_4)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042ea10
+void FUN_0042ea10(int32 param_1, int32 param_2, int32 param_3, uint32 param_4)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042eaf0
+void FUN_0042eaf0(struct GXObject* param_1, struct GXObject* param_2, struct GXObject* param_3)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042f5f0
+void FUN_0042f5f0(struct GXObject* param_1, BOOL param_2)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042f720
+void FUN_0042f720(struct GXObject* param_1)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042f7e0
+static void FUN_0042f7e0(struct GXObject *param_1) // DoIt for ob spawned in FUN_0042f800
+{
+    UNIMPLEMENTED;
+}
+
+// 0042f800
+void FUN_0042f800(struct GXObject* param_1, int32 x, int32 y)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042f860
+void FUN_0042f860(struct GXObject* param_1)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042f910
+void FUN_0042f910(struct GXObject* param_1, uint32 param_2)
+{
+    UNIMPLEMENTED;
+}
+
+// 0042ff10
+void ob229Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00430060
+int32 ob229_00430060(int16* param_1)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 00430090
+void ob229DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004308d0
+void ob229Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00430990
+BOOL ob229_00430990(struct GXObject* param_1, int param_2, int param_3, int param_4, int param_5, int param_6, int param_7, int param_8, int param_9)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 00430b40
+BOOL ob229_00430b40(void)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 00430c50
+void ob229Clid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 00430d70
+void ob230Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00430de0
+void ob231Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00430e20
+void ob231DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00430ea0
+void ob231Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00430f20
+void ob232DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431220
+void ob232Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431280
+void ob233Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431300
+void ob233DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004313d0
+void ob234Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431470
+void ob234DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431640
+void ob234Clid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 004316d0
+void ob240Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 004316e0
+void ob240DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004316f0
+void ob240Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431700
+void ob291Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431710
+void ob291DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431720
+void ob291Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431730
+void FUN_00431730(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004317b0
+void GOB_UpdatePositionRelativeToParent(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004317e0
+void GOB_ResetPos(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431820
+static BOOL FUN_00431820(void)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 00431830
+void FUN_00431830(struct GXObject* param_1, int32 x, int32 y)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431900
+BOOL FUN_00431900(struct GXObject* param_1)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 00431990
+void FUN_00431990(struct GXObject* param_1, BOOL param_2)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431dc0
+void ob93Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431e00
+void ob93DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431f90
+void ob93Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00431fa0
+BOOL ob93_00431fa0(struct GXObject* param_1, BOOL param_2)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+// 004322a0
+void ob93_004322a0(struct GXObject* param_1)
+{
+    UNIMPLEMENTED;
+}
+
+// 004322b0
+void ob93_004322b0(struct GXObject* param_1, uint32 param_2)
+{
+    UNIMPLEMENTED;
+}
+
+// 00432390
+void ob93Clid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 00432650
+void ob94Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00432680
+void ob94DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004327e0
+void ob94Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004327f0
+void ob94_004327f0(struct GXObject* param_1)
+{
+    UNIMPLEMENTED;
+}
+
+// 00432930
+void ob94_00432930(struct GXObject* param_1)
+{
+    UNIMPLEMENTED;
+}
+
+// 004329f0
+void ob94Clid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 00432a70
+void ob95Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00432ab0
+void ob95DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00432c80
+void ob95Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00432c90
+void FUN_00432c90(struct GXObject* param_1)
+{
+    UNIMPLEMENTED;
+}
+
+// 00432d10
+void FUN_00432d10(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 0432d40
+void FUN_00432d40(struct GXObject* param_1)
+{
+    UNIMPLEMENTED;
+}
+
+// 00432e60
+void FUN_00432e60(struct GXObject* param_1)
+{
+    UNIMPLEMENTED;
+}
+
+// 0432f30
+void FUN_00432f30(struct GXObject* param_1)
+{
+    UNIMPLEMENTED;
+}
+
+// 00432f40
+void FUN_00432f40(struct GXObject* param_1)
+{
+    UNIMPLEMENTED;
+}
+
+// 00432fa0
+void FUN_00432fa0(struct GXObject* gob, struct CollideInfo* param_2)
+{
+    UNIMPLEMENTED;
+}
+
+// 004330c0
+void FUN_004330c0(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00433140
+void FUN_00433140(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00433170
+void ob95Clid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 00433370
+void ob369_00433370(void)
+{
+    UNIMPLEMENTED;
+}
+
+// 00433380
+void ob369Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 004333c0
+void ob369DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004334c0
+void ob369Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00433520
+void ob369Clid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 00433590
+uint8* SCRIPT_DoEvent(struct GXObject* gob, struct GXAniScript* script, uint8* scriptPtr, int32 eventType)
+{
+    UNIMPLEMENTED;
+    return NULL;
+}
+
+// 004335f0
+void DefInit(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00433670
+void EnemyInit(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00433690
+void DefHit(struct GXObject* param_1, struct CollideInfo* param_2)
+{
+    UNIMPLEMENTED;
+}
+
+// 004338d0
+void EnemyClid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 00433900
+void GOB_ProcessEvents(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004339c0
+void DefDoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00433a70
+void EnemyDoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00433b20
+void DefDraw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00433e60
+void ExitTVDraw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00433ec0
+void KFBoss_00433ec0(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00434080
+void KFBossDraw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00434190
+void FUN_00434190(struct GXObject* gob, int32 param_2)
+{
+    UNIMPLEMENTED;
+}
+
+// 00434260
+void FUN_00434260(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004344a0
+void FUN_004344a0(void)
+{
+    UNIMPLEMENTED;
+}
+
+// 004344b0
+void FUN_004344b0(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 004344d0
+void FUN_004344d0(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00434540
+void FUN_00434540(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00434550
+void FUN_00434550(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00434570
+void PlatformDoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00434670
+void PlatformDraw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00434a20
+void EnemyInitPath(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00434a50
+void FUN_00434a50(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00434a60
+void FUN_00434a60(struct GXObject* param_1, struct GXObject* param_2)
+{
+    UNIMPLEMENTED;
+}
+
+// 00434ad0
+void FUN_00434ad0(struct GXObject* param_1, struct GXObject* param_2)
+{
+    UNIMPLEMENTED;
+}
+
+// 00434b10
+void CLID_GenericPlat(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 004355d0
+void FUN_04355d0(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 004356e0
+void ob2Clid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
+
+// 00435760
+void ob2DoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00435980
+void ob2Draw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00435a10
+void FUN_00435a10(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00435ab0
+void ob2Init(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00435b90
+void CollectibleInit(struct GXObject* gob, BOOL removed)
+{
+    UNIMPLEMENTED;
+}
+
+// 00435ba0
+void CollectibleDoIt(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00435cb0
+void CollectibleDraw(struct GXObject* gob)
+{
+    UNIMPLEMENTED;
+}
+
+// 00435cc0
+void CollectibleClid(struct GXObject* gob, struct CollideInfo* ci)
+{
+    UNIMPLEMENTED;
+}
